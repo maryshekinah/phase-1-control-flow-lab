4e33254c417ab8ba9ebd629362bd7037a332b4d4
shekinah <nyamburamary1212@gmail.com>
2023-09-18 17:45:08 +0300

    if statement

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
+    return 'This one is on me!';
+  } else if (distance > 2000) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'Please input a valid distance.';
+  }
 }
+
 
 function ternaryCheckCity(){
   // Write your code here!
